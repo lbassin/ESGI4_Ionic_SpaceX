957fe7da01fe327b8ccfd755bf293b27b8c943c4
xRaz <maxime.marquet1@gmail.com>
2018-05-26 17:01:16 +0200

    #6 observable countdown

--- src/pages/launches/launches.ts
+++ src/pages/launches/launches.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+
+const counter = Observable.interval(1000);
 import { ApiService } from '../../providers/api.service';
 import { ILaunch } from '../../app/models/ILaunch';
 
@@ -13,11 +17,20 @@ export class LaunchesPage {
     public nextLaunch: ILaunch;
     public launches: ILaunch[];
 
+    public countdown: number;
+    public currentTimestamp = Math.round(+new Date()/1000);
+
     constructor(public navCtrl: NavController, public navParams: NavParams, private spacexApi: ApiService) {
 
         spacexApi.getAllLaunches().subscribe(data => {
             this.nextLaunch = data[0];
-            this.launches = data;
+            this.launches = data.reverse();
+
+            counter.subscribe(() => {
+                this.countdown = Math.round(+new Date()/1000) - this.nextLaunch.launch_date_unix;
+            });
         });
     }
+
+    ionViewDidLoad() { }
 }
