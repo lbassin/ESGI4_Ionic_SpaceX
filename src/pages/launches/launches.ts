8bca9028da2d1e179dee2e541a2031810a04bd10
xRaz <maxime.marquet1@gmail.com>
2018-05-28 15:40:47 +0200

    #6 #7 countdown finished, card tabs finished

--- src/pages/launches/launches.ts
+++ src/pages/launches/launches.ts
@@ -14,23 +14,64 @@ import { ILaunch } from '../../app/models/ILaunch';
 })
 export class LaunchesPage {
 
-    public nextLaunch: ILaunch;
-    public launches: ILaunch[];
+    public typeLaunches: string = "upcoming";
 
-    public countdown: number;
-    public currentTimestamp = Math.round(+new Date()/1000);
+    public nextLaunch: ILaunch;
+    public nextLaunchDays: number = 0;
+    public nextLaunchHours: number = 0;
+    public nextLaunchMinutes: number = 0;
+    public nextLaunchSeconds: number = 0;
+
+    public upcomingLaunches: ILaunch[] = [];
+    public pastLaunches: ILaunch[] = [];
 
     constructor(public navCtrl: NavController, public navParams: NavParams, private spacexApi: ApiService) {
 
         spacexApi.getAllLaunches().subscribe(data => {
-            this.nextLaunch = data[0];
-            this.launches = data.reverse();
+            let reverseData = data.reverse();
+
+            reverseData.forEach(element => {
+                if (element.launch_date_unix <= Math.round(+new Date() / 1000)) {
+                    this.pastLaunches.push(element);
+                } else {
+                    if (element.links.mission_patch_small === null) {
+                        element.links.mission_patch_small = "http://freedesignfile.com/upload/2017/08/rocket-icon-vector.png"
+                    }
+                    this.upcomingLaunches.push(element);
+                }
+            });
+
+            this.upcomingLaunches.reverse();
+            this.nextLaunch = this.upcomingLaunches[0];
+            this.upcomingLaunches.shift();
+
+            let countdown = new Date();
+
+            countdown.setDate(countdown.getDate() + (new Date(this.nextLaunch.launch_date_utc).getDate() - countdown.getDate()));
+            countdown.setHours(countdown.getHours() + (new Date(this.nextLaunch.launch_date_utc).getHours() - countdown.getHours()));
+            countdown.setMinutes(countdown.getMinutes() + (new Date(this.nextLaunch.launch_date_utc).getMinutes() - countdown.getMinutes()));
+            countdown.setSeconds(countdown.getSeconds() + (new Date(this.nextLaunch.launch_date_utc).getSeconds() - countdown.getSeconds()));
 
             counter.subscribe(() => {
-                this.countdown = Math.round(+new Date()/1000) - this.nextLaunch.launch_date_unix;
+                this.amazingCountdownFunction(countdown);
             });
         });
     }
 
     ionViewDidLoad() { }
+
+    private amazingCountdownFunction(toDate) {
+        let dateEntered = toDate;
+        let now = new Date();
+        let difference = dateEntered.getTime() - now.getTime();
+
+        this.nextLaunchSeconds = Math.floor(difference / 1000);
+        this.nextLaunchMinutes = Math.floor(this.nextLaunchSeconds / 60);
+        this.nextLaunchHours = Math.floor(this.nextLaunchMinutes / 60);
+        this.nextLaunchDays = Math.floor(this.nextLaunchHours / 24);
+
+        this.nextLaunchHours %= 24;
+        this.nextLaunchMinutes %= 60;
+        this.nextLaunchSeconds %= 60;
+    }
 }
